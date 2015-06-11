39e35d948362d591d3ccd44c60d2e9af39f7c812
Said Maadan <saidmaadan@users.noreply.github.com>
2015-06-10 20:27:34 -0500

    Create routes for signin and signup users

--- app.js
+++ app.js
@@ -23,13 +23,57 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Handles file uploads
+app.use(multer({dest:'./uploads'}));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//Handle Express Sessions
+app.use(session({
+  secret:'secret',
+  saveUninitialized:true,
+  resave:true
+}));
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Validator
+app.use(expressValidator({
+  errorFormatter: function(param,msg,value){
+    var namespace = param.split('.')
+    var root = namespace.shift()
+    var formparam = root;
+
+  while(namespace.length){
+    formParam += '[' + namespace.shift()+ ']';
+  }
+  return{
+    param:formParam,
+    msg:msg,
+    value:value
+  };
+  }
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
