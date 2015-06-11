b61b10dd28d00f87dc121f52b1aed6aca71f62f7
Said Maadan <saidmaadan@users.noreply.github.com>
2015-06-10 21:23:34 -0500

    Create users model and add twitter bootstrap to enhance the visual appearance

---
+++ models/user.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://localhost/loginSystem');
+
+var db = mongoose.connection;
+
+//User Schema
+var UserSchema = mongoose.Schema({
+	username:{
+		type: String,
+		index:true
+	},
+	password:{
+		type:String, required:true, bcrypt:true
+	},
+	email:{
+		type:String
+	},
+	name:{
+		type:String
+	},
+	profileimage:{
+		type:String
+	}
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch){
+		if(err) return callback(err);
+		callback(null, isMatch);
+	});
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+
+module.exports.createUser = function(newUser, callback) {
+	bcrypt.hash(newUser.password, 10, function(err, hash){
+		if(err) throw err;
+		// Set hashed pw
+		newUser.password = hash;
+		// Create User
+		newUser.save(callback)
+	});
+}
